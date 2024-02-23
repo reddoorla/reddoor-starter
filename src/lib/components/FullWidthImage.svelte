622bee652a5472c2e2b8361070a8b0d5567332ef
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2024-02-23 16:40:45 -0700

    forcing tailwind to load classes

--- src/lib/components/FullWidthImage.svelte
+++ src/lib/components/FullWidthImage.svelte
@@ -7,6 +7,7 @@ export let placeholderSide = "right"
 </script>
 
 <section>
+	<div class="right-0 left-0"></div>
 	<div  class="h-[160vw] sm:h-[90vw] xl:h-[60vw] md:max-h-screen relative w-full">
 		<img src={image} alt={altText} class="absolute bottom-0 {placeholderSide}-0 h-full w-full object-cover {image===placeholder ? "lg:w-[45%] md:h-auto" : ""} -z-10"/>
         <div class='w-full h-full max-w-[1280px] mx-auto p-[4%] xl:p-0'>
