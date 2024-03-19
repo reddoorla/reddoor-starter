859e245bdfe48af432d0f4015fecd2cdbdf3a15a
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2024-03-19 16:49:24 -0400

    team pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,6 +37,7 @@ export default {
       transitionTimingFunction: {
         'in-expo': 'cubic-bezier(0.95, 0.05, 0.795, 0.035)',
         'out-expo': 'cubic-bezier(0.19, 1, 0.22, 1)',
+        'fast-slow':'cubic-bezier(.5,0,0,1)'
       }
     },
   },
