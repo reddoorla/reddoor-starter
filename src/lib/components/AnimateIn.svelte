931fca66a3d78acdc78f26011678dfef5b727029
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2024-09-11 10:56:25 -0700

    animate in added

---
+++ src/lib/components/AnimateIn.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+  
+      let isInView = false;
+      let el:HTMLElement | null;
+      let transitionDelay = 0;
+  
+      export let transitionDelayMax = 400;
+      export let style = "";
+  
+  
+      onMount(()=>{
+          if(el){
+              let rect = el.getBoundingClientRect();
+              isInView = rect.bottom <= window.innerHeight + rect.height
+              transitionDelay= transitionDelayMax * (rect.left/window.innerWidth)
+          }
+  
+          window.addEventListener('scroll', ()=>{
+              if (el) { 
+                  let rect = el.getBoundingClientRect();
+                  isInView = rect.bottom <= window.innerHeight + rect.height
+              }
+          
+          })
+      })
+  </script>
+  
+  
+     
+  
+          <div bind:this={el} class="transition duration-[2000ms] ease-fast-slow {isInView ? "opacity-100 translate-y-0":"opacity-0 translate-y-[50%]"} {$$props.class || ''}" style="transition-delay:{transitionDelay}ms; {style}">
+              <slot />
+          </div>
+  
