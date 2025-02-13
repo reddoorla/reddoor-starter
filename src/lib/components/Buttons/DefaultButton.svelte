8bed1a75ee4a2758b23450273864a7d95995a843
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2025-02-13 13:51:22 -0800

    updated default button

--- src/lib/components/Buttons/DefaultButton.svelte
+++ src/lib/components/Buttons/DefaultButton.svelte
@@ -1,23 +1,34 @@
 <script lang="ts">
     let { href = "", onclick, class:passedClasses = '', children, ...others  } = $props();
 
-    const baseClasses = "rounded border-2 border-solid border-dark px-10 pt-4 pb-3 h-fit";
+    const baseClasses = "rounded border-2 border-solid border-dark px-10 pt-4 pb-3 h-fit hover:bg-dark hover:text-white transition"
 
 </script>
 
+<style>
+    a, button{
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+}
+</style>
 
 
-{#if href}
-    <a 
-        {href} 
-        {onclick} 
-        class="{baseClasses} {passedClasses}">
-        {@render children()}
-    </a>
-{:else}
-    <button 
-        {onclick}
-        class="{baseClasses} {passedClasses}">
-        {@render children()}
+<div class="bump">
+    {#if href}
+        <a 
+            {href} 
+            {onclick} 
+            class="{baseClasses} {passedClasses}">
+                {@render children()}
+        </a>
+    {:else}
+    <button {onclick}
+            class="{baseClasses} {passedClasses}">
+            {@render children()}
     </button>
-{/if}
+    {/if}
+</div>
