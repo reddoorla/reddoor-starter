a21044a0b37e3f5b8afa959913b825646abf5b6d
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2025-10-29 10:42:24 -0700

    type updates

--- .svelte-kit/non-ambient.d.ts
+++ .svelte-kit/non-ambient.d.ts
@@ -23,3 +23,25 @@ declare module "svelte/elements" {
 }
 
 export {};
+
+
+declare module "$app/types" {
+	export interface AppTypes {
+		RouteId(): "/" | "/api" | "/api/preview" | "/slice-simulator" | "/[[preview=preview]]" | "/[[preview=preview]]/[uid]";
+		RouteParams(): {
+			"/[[preview=preview]]": { preview?: string };
+			"/[[preview=preview]]/[uid]": { preview?: string; uid: string }
+		};
+		LayoutParams(): {
+			"/": { preview?: string; uid?: string };
+			"/api": Record<string, never>;
+			"/api/preview": Record<string, never>;
+			"/slice-simulator": Record<string, never>;
+			"/[[preview=preview]]": { preview?: string; uid?: string };
+			"/[[preview=preview]]/[uid]": { preview?: string; uid: string }
+		};
+		Pathname(): "/" | "/api" | "/api/" | "/api/preview" | "/api/preview/" | "/slice-simulator" | "/slice-simulator/" | `${string}` & {} | `${string}/` & {} | `${string}/${string}` & {} | `${string}/${string}/` & {};
+		ResolvedPathname(): `${"" | `/${string}`}${ReturnType<AppTypes['Pathname']>}`;
+		Asset(): "/favicon.png" | "/robots.txt" | string & {};
+	}
+}
